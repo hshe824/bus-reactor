87f4e0dab08cda62d821bec31bf9e2db9265a7b7
hshe824 <hshe824@aucklanduni.ac.nz>
2018-02-24 23:29:50 -0800

    Asynchronously fetch bus location from Translink API

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+// import superagent from 'superagent';
 import MapGL from 'react-map-gl';
+
+const request = require('superagent');
+
+const ACCESS_TOKEN = 'pk.eyJ1IjoicnlzaG56IiwiYSI6ImNqZTIwbWljdTFlOXMycXFseXdoZTdhMHoifQ.EaRv0yYowqeNviNdcgL-PQ' // Mapbox access token
+const TRANSLINK_API_URL ='http://api.translink.ca/rttiapi/v1/buses?apikey=iE0h8jkaEpNFmV7PrYXG';
 
 class Map extends Component {
 
   constructor() {
     super()
-    this.accessToken = 'pk.eyJ1IjoicnlzaG56IiwiYSI6ImNqZTIwbWljdTFlOXMycXFseXdoZTdhMHoifQ.EaRv0yYowqeNviNdcgL-PQ' // Mapbox access token
     this.state = {
       viewport: {
-        width: 400,
-        height: 400,
-        latitude: 49.2827,
-        longitude: -123.1207,
-        zoom: 8
+        width: 1380,
+        height: 720,
+        latitude: 49.237368,
+        longitude: -123.117362,
+        zoom: 11
       }
     };
   }
@@ -25,18 +30,41 @@ class Map extends Component {
         <MapGL
           {...viewport}
           style={{ width: '400px', height: '400px' }}
-          mapboxApiAccessToken={this.accessToken}
+          mapboxApiAccessToken={ACCESS_TOKEN}
           mapStyle="mapbox://styles/mapbox/streets-v10"
-          onChangeViewport={viewport => {
-            const {latitude, longitude, zoom} = viewport;
+          onViewportChange={viewport => {
             this.setState({ viewport })
-            // Optionally call `setState` and use the state to update the map. 
-          }}
-          // onViewportChange={viewport => {
-          //   this.setState({ viewport })
-          // }}
+          }
+          }
         />
-      </div>)
+      </div>)   
+  }
+
+  componentDidMount() {
+    this.getBusLocations()
+  }
+
+  
+
+  processBusLocations(body){
+    var busData = this.parseJSON(body)
+   
+  }
+
+
+  parseJSON(json){
+    console.log(json)
+  }
+
+
+  getBusLocations() {
+     request
+    .get(TRANSLINK_API_URL)
+    .set('Accept', 'application/json')
+    .buffer(true)
+    .end((err, res) => {
+      this.processBusLocations(res.body)
+   });
   }
 }
 
