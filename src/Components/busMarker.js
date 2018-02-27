f9eace6da890b2923b88b64e81d60c1344992004
hshe824 <hshe824@aucklanduni.ac.nz>
2018-02-26 21:02:24 -0800

    Fixed css strange behaviour with rendering of buses

--- src/Components/busMarker.js
+++ src/Components/busMarker.js
@@ -1,4 +1,5 @@
 import React, {PureComponent} from 'react';
+import '../App.css';
 
 const pinStyle = {
   cursor: 'pointer',
@@ -10,12 +11,18 @@ export default class BusMarker extends PureComponent {
 
   render() {
     const {size=20,onClick, direction} = this.props;
+    let className = "businfoShort";
+
+    if (direction=="NORTH" || direction == "SOUTH"){
+      className = "businfoLong";
+    }
 
     return (
-      <img className="busicon" alt='' height={size} width ={size*2}
-        style={{...pinStyle, transform: `translate(${-size/2}px,${-size}px)`}}
+      <img className={className} alt='' 
         onClick={onClick} src={require('../icons/bus_' +direction+ '.png')}> 
       </img>
     );
+    //height={size} width ={size*2}
+    //style={{...pinStyle, transform: `translate(${-size/2}px,${-size}px)`}}
   }
 }
